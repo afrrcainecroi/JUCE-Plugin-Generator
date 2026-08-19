ce994bd9bed3cd8734a1182c84afc4e9bd433a2b
franco <afrrcainecroi@gmail.com>
2026-08-19 16:53:34 +0200

    Extract DSL model module

--- generator-app/code-generator.scm
+++ generator-app/code-generator.scm
@@ -87,6 +87,7 @@
   #:use-module (generator-app generation-state)
   #:use-module (generator-app resources)
   #:use-module (generator-app layout)
+  #:use-module (generator-app dsl-model)
   #:re-export (register-image-set!
                register-component!
                validate-component!
@@ -122,7 +123,123 @@
                register-screen!
                generate-layout-data-components
                generate-screen-size-code
-               generate-grid-code)
+               generate-grid-code
+               <component>
+               component:id
+               component:role
+               component:row
+               component:col
+               component:row-span
+               component:col-span
+               component:margin-tb
+               component:margin-lr
+               <label>
+               label:text
+               label:justification
+               <header>
+               <footer>
+               <link>
+               link:url
+               <palette-label>
+               palette-label:enable
+               palette-label:default-theme
+               <selector>
+               selector:items
+               selector:default-index
+               <palette-selector>
+               <button>
+               button:text
+               <text-button>
+               <toggle-button>
+               toggle-button:default-state
+               toggle-button:style
+               toggle-button:parameter-id
+               toggle-button:parameter-name
+               toggle-button:tooltip
+               toggle-button:processor-reference
+               toggle-button:version-hint
+               <normal-toggle-button>
+               <switch>
+               <bypass-switch>
+               <slider>
+               slider:parameter-id
+               slider:parameter-name
+               slider:processor-reference
+               slider:version-hint
+               slider:title
+               slider:min
+               slider:max
+               slider:default
+               slider:interval
+               slider:scale
+               slider:value-type
+               slider:suffix
+               slider:show-value
+               slider:show-ticks
+               slider:show-labels
+               slider:tick-count
+               slider:tick-mode
+               slider:tick-labels
+               <rotary-slider>
+               rotary-slider:icon-type
+               rotary-slider:morph-icon
+               rotary-slider:icon-set
+               <linear-slider>
+               linear-slider:orientation
+               <meter>
+               meter:style
+               meter:scale-type
+               meter:is-sharp
+               meter:glow-multiplier
+               meter:range-min
+               meter:range-max
+               meter:num-segments
+               meter:tick-mode
+               <scope>
+               scope:grid-style
+               scope:is-sharp
+               scope:glow-multiplier
+               <header-footer>
+               header-footer:id
+               header-footer:title-header
+               header-footer:row-header
+               header-footer:col-header
+               header-footer:row-span-header
+               header-footer:col-span-header
+               header-footer:margin-tb-header
+               header-footer:margin-lr-header
+               header-footer:title-footer
+               header-footer:row-footer
+               header-footer:col-footer
+               header-footer:row-span-footer
+               header-footer:col-span-footer
+               header-footer:margin-tb-footer
+               header-footer:margin-lr-footer
+               header-footer:title-link
+               header-footer:url-link
+               header-footer:row-link
+               header-footer:col-link
+               header-footer:row-span-link
+               header-footer:col-span-link
+               header-footer:margin-tb-link
+               header-footer:margin-lr-link
+               <palette>
+               palette:id
+               palette:enable
+               palette:default-theme
+               palette:title-palette
+               palette:row-palette
+               palette:col-palette
+               palette:row-span-palette
+               palette:col-span-palette
+               palette:margin-tb-palette
+               palette:margin-lr-palette
+               palette:row-selector
+               palette:col-selector
+               palette:row-span-selector
+               palette:col-span-selector
+               palette:margin-tb-selector
+               palette:margin-lr-selector)
   )
 
 ;; COMPONENT MODEL
@@ -138,307 +255,6 @@
     output-meter
     scope))
 
-(new-class <component>
-	   (
-	    ;; Identificatore logico stabile del componente
-	    (id #f)
-	    (role #f)
-	    ;; Layout.
-	    ;; In futuro row/col potranno essere calcolati dal layout solver.
-	    (row #f)
-	    (col #f)
-	    (row-span 1)
-	    (col-span 1)
-	    (margin-tb 0)
-	    (margin-lr 0))
-	   #:code
-	   #t)
-
-(new-class <label> (<component>)
-	   (
-	    (text "")
-	    (justification 'centred))
-	   #:code
-	   (register-component! this))
-
-(new-class <selector> (<component>)
-	   (
-	    (items '())
-	    (default-index 0))
-	   #:code
-	   (register-component! this))
-
-(new-class <button> (<component>)
-	   (
-	    (text ""))
-	   #:code
-	   #t)
-
-
-(new-class <text-button> (<button>)
-	   ()
-	   #:code
-	   (register-component! this))
-
-
-(new-class <toggle-button> (<button>)
-	   (
-	    (default-state #f)
-	    (style 'normal)
-	    ;; binding DAW/APVTS
-	    (parameter-id #f)
-	    (parameter-name #f)
-	    (tooltip "")
-	    (processor-reference #f)
-	    (version-hint 1))
-	   #:code
-	   #t)
-
-
-(new-class <switch> (<toggle-button>)
-	   ()
-	   #:code
-	   (slot-set! this 'style 'switch)
-	   (register-component! this))
-
-(new-class <normal-toggle-button> (<toggle-button>)
-	   ()
-	   #:code
-	   (register-component! this))
-
-
-(new-class <meter> (<component>)
-	   (
-	    ;; 'segmented oppure 'analog
-	    (style 'segmented)
-	    ;; proprietà grafiche del KineticMeter
-	    (scale-type 'default)
-	    (is-sharp #f)
-	    (glow-multiplier 1.0)
-	    (range-min -60.0)
-	    (range-max 6.0)
-	    (num-segments 20)
-	    (tick-mode 'all))
-	   #:code
-	   (register-component! this))
-
-
-(new-class <scope> (<component>)
-	   (
-	    ;; proprietà grafiche del KineticScope
-	    (grid-style 'default)
-	    (is-sharp #f)
-	    (glow-multiplier 1.0))
-	   #:code
-	   (register-component! this))
-
-;; ======================================================================
-;; SLIDER BASE CLASS
-;; ======================================================================
-(new-class <slider> (<component>)
-	   (
-	    (parameter-id #f)
-	    (parameter-name #f)
-	    (processor-reference #f)
-	    (version-hint 1)
-	    (title "")
-	    (min 0.0)
-	    (max 1.0)
-	    (default 0.0)
-	    (interval 0.0)
-	    (scale 'linear)
-	    (value-type 'default)
-	    (suffix "")
-	    (show-value #t)
-	    (show-ticks #f)
-	    (show-labels #f)
-	    (tick-count 0)
-	    (tick-mode 'all)
-	    (tick-labels '()))
-	   #:code
-	   #t)
-
-;; ======================================================================
-;; ROTARY SLIDER
-;; ======================================================================
-(new-class <rotary-slider> (<slider>)
-	   (
-	    (icon-type -1)
-	    (morph-icon #f)
-	    (icon-set ""))
-	   #:code
-	   (register-component! this))
-
-;; ======================================================================
-;; LINEAR SLIDER
-;; ======================================================================
-(new-class <linear-slider> (<slider>)
-	   ((orientation 'horizontal))
-	   #:code
-	   (register-component! this))
-
-(new-class <header> (<label>)
-  ()
-  #:code
-  #t)
-
-(new-class <footer> (<label>)
-  ()
-  #:code
-  #t)
-
-(new-class <link> (<label>)
-  ((url ""))
-  #:code
-  #t)
-
-(new-class <palette-label> (<label>)
-  ((enable #t)
-   (default-theme 3))
-  #:code
-  #t)
-
-
-;; header e footer
-(new-class <header-footer>
-	   (
-	    ;; Identificatore logico del composito
-	    (id "Main Header Footer")
-	    ;; Header
-	    (title-header "YAPlugin")
-	    (row-header 1)
-	    (col-header 8)
-	    (row-span-header 1)
-	    (col-span-header 8)
-	    (margin-tb-header 0)
-	    (margin-lr-header 0)
-	    ;; Footer
-	    (title-footer "Copyright (c) 2025 AF-Audio")
-	    (row-footer 15)
-	    (col-footer 20)
-	    (row-span-footer 1)
-	    (col-span-footer 4)
-	    (margin-tb-footer 12)
-	    (margin-lr-footer 0)
-	    ;; Link
-	    (title-link "https://www.aacf-music.eu/")
-	    (url-link "https://www.aacf-music.eu/")
-	    (row-link 15)
-	    (col-link 1)
-	    (row-span-link 1)
-	    (col-span-link 5)
-	    (margin-tb-link 0)
-	    (margin-lr-link 0))
-	   #:code
-	   (let ((base id))
-	     (make <header>
-               #:id
-               (string-append base " Header")
-               #:text title-header
-               #:row row-header
-               #:col col-header
-               #:row-span row-span-header
-               #:col-span col-span-header
-               #:margin-tb margin-tb-header
-               #:margin-lr margin-lr-header)
-	     (make <footer>
-               #:id
-               (string-append base " Footer")
-               #:text title-footer
-               #:row row-footer
-               #:col col-footer
-               #:row-span row-span-footer
-               #:col-span col-span-footer
-               #:margin-tb margin-tb-footer
-               #:margin-lr margin-lr-footer)
-	     (make <link>
-               #:id
-               (string-append base " Link")
-               #:text title-link
-               #:url url-link
-               #:row row-link
-               #:col col-link
-               #:row-span row-span-link
-               #:col-span col-span-link
-               #:margin-tb margin-tb-link
-               #:margin-lr margin-lr-link)))
-
-(define *kinetic-palettes*
-  '("Cyan (Cyberpunk)"
-    "Plasma (Purple)"
-    "Gold (Amber)"
-    "Matrix (Green)"
-    "Fire (Red)"
-    "Ocean (Blue)"
-    "Toxic (Lime)"
-    "Radon (Pink)"
-    "White (Mono)"
-    "Midnight (Dark)"
-    "Sunset (Orange)"
-    "Mint (Teal)"
-    "Vaporwave (Pink)"
-    "Amber (Amber)"
-    "Crimson (Red)"
-    "Voltage (Yellow)"
-    "Ultraviolet (Violet)"
-    "Stealth (Grey)"))
-
-(new-class <palette-selector> (<selector>)
-	   ()
-	   #:code
-	   #t)
-
-(new-class <palette>
-	   (
-	    (id "Main Palette")
-	    (enable #t)
-	    (default-theme 3)
-	    (title-palette "Theme")
-	    (row-palette 1)
-	    (col-palette 20)
-	    (row-span-palette 1)
-	    (col-span-palette 2)
-	    (margin-tb-palette 12)
-	    (margin-lr-palette 0)
-	    (row-selector 1)
-	    (col-selector 22)
-	    (row-span-selector 1)
-	    (col-span-selector 3)
-	    (margin-tb-selector 10)
-	    (margin-lr-selector 4))
-	   #:code
-	   (when enable
-	     (make <palette-label>
-               #:id
-               (string-append id " Label")
-               #:text title-palette
-               #:enable enable
-               #:default-theme default-theme
-               #:justification 'centred-right
-               #:row row-palette
-               #:col col-palette
-               #:row-span row-span-palette
-               #:col-span col-span-palette
-               #:margin-tb margin-tb-palette
-               #:margin-lr margin-lr-palette)
-	     (make <palette-selector>
-               #:id
-               (string-append id " Selector")
-               #:items *kinetic-palettes*
-               ;; selector usa indice 0-based
-               #:default-index default-theme
-               #:row row-selector
-               #:col col-selector
-               #:row-span row-span-selector
-               #:col-span col-span-selector
-               #:margin-tb margin-tb-selector
-               #:margin-lr margin-lr-selector)))
-
-(new-class <bypass-switch> (<switch>)
-  ()
-  #:code
-  #t)
-
 (define-public (processor-param-var model)
   (string-append
    "param_"
@@ -452,185 +268,6 @@
 (define-public (processor-reference model)
   (assoc-ref model 'processor-reference))
 
-;; ======================================================================
-;; COMPONENT TYPE
-;;
-;; Restituisce il tipo semantico concreto del componente.
-;; ======================================================================
-(define-method (component-type (c <component>))
-  'component)
-
-(define-method (component-type (s <slider>))
-  'slider)
-
-(define-method (component-type (s <rotary-slider>))
-  'rotary-slider)
-
-(define-method (component-type (s <linear-slider>))
-  'linear-slider)
-
-(define-method (component-type (c <label>))
-  'label)
-
-(define-method (component-type (c <selector>))
-  'selector)
-
-(define-method (component-type (b <button>))
-  'button)
-
-(define-method (component-type (b <text-button>))
-  'text-button)
-
-(define-method (component-type (b <toggle-button>))
-  'toggle-button)
-
-(define-method (component-type (b <normal-toggle-button>))
-  'toggle-button)
-
-(define-method (component-type (b <switch>))
-  'switch)
-
-(define-method (component-type (m <meter>))
-  'meter)
-
-(define-method (component-type (s <scope>))
-  'scope)
-
-(define-method (component-type (c <header>))
-  'header)
-
-(define-method (component-type (c <footer>))
-  'footer)
-
-(define-method (component-type (c <link>))
-  'link)
-
-(define-method (component-type (c <palette-label>))
-  'palette-label)
-
-(define-method (component-type (c <palette-selector>))
-  'palette-selector)
-
-(define-method (component-type (b <bypass-switch>))
-  'bypass-switch)
-
-
-;; ======================================================================
-;; COMPONENT -> MODEL
-;;
-;; Trasforma un oggetto GOOPS nel modello intermedio.
-;;
-;; IMPORTANTE:
-;; questa operazione NON alloca il nome della variabile C++.
-;; component->model deve rimanere priva di quell'effetto collaterale.
-;; ======================================================================
-(define-method (component->model (c <component>))
-  `((id        . ,(component:id c))
-    (type      . ,(component-type c))
-    (role      . ,(component:role c))
-    (row       . ,(component:row c))
-    (col       . ,(component:col c))
-    (rowSpan   . ,(component:row-span c))
-    (colSpan   . ,(component:col-span c))
-    (margin-tb . ,(component:margin-tb c))
-    (margin-lr . ,(component:margin-lr c))))
-
-(define-method (component->model (b <button>))
-  (append
-   (next-method)
-   `((text . ,(button:text b)))))
-
-(define-method (component->model (b <toggle-button>))
-  (append
-   (next-method)
-   `((default-state       . ,(toggle-button:default-state b))
-     (style               . ,(toggle-button:style b))
-     (parameter-id        . ,(toggle-button:parameter-id b))
-     (parameter-name      . ,(toggle-button:parameter-name b))
-     (processor-reference . ,(toggle-button:processor-reference b))
-     (version-hint        . ,(toggle-button:version-hint b))
-     (tooltip             . ,(toggle-button:tooltip b)))))
-
-(define-method (component->model (c <label>))
-  (append
-   (next-method)
-   `((text          . ,(label:text c))
-     (justification . ,(label:justification c)))))
-
-(define-method (component->model (c <link>))
-  (append
-   (next-method)
-   `((url . ,(link:url c)))))
-
-(define-method (component->model (c <palette-label>))
-  (append
-   (next-method)
-   `((enable        . ,(palette-label:enable c))
-     (default-theme . ,(palette-label:default-theme c)))))
-
-(define-method (component->model (c <selector>))
-  (append
-   (next-method)
-   `((items         . ,(selector:items c))
-     (default-index . ,(selector:default-index c)))))
-
-(define-method (component->model (s <slider>))
-  (append
-   (next-method)
-   `((parameter-id        . ,(slider:parameter-id s))
-     (parameter-name      . ,(slider:parameter-name s))
-     (processor-reference . ,(slider:processor-reference s))
-     (version-hint        . ,(slider:version-hint s))
-     (title               . ,(slider:title s))
-     (min                 . ,(slider:min s))
-     (max                 . ,(slider:max s))
-     (default             . ,(slider:default s))
-     (interval            . ,(slider:interval s))
-     (scale               . ,(slider:scale s))
-     (value-type          . ,(slider:value-type s))
-     (suffix              . ,(slider:suffix s))
-     (show-value          . ,(slider:show-value s))
-     (show-ticks          . ,(slider:show-ticks s))
-     (show-labels         . ,(slider:show-labels s))
-     (tick-count          . ,(slider:tick-count s))
-     (tick-mode           . ,(slider:tick-mode s))
-     (tick-labels         . ,(slider:tick-labels s)))))
-
-(define-method (component->model (s <rotary-slider>))
-  (append
-   (next-method)
-   `((icon-type  . ,(rotary-slider:icon-type s))
-     (morph-icon . ,(rotary-slider:morph-icon s))
-     (icon-set   . ,(rotary-slider:icon-set s)))))
-
-(define-method (component->model (s <linear-slider>))
-  (append
-   (next-method)
-   `((orientation . ,(linear-slider:orientation s)))))
-
-(define-method (component->model (m <meter>))
-  (append
-   (next-method)
-   `((style           . ,(meter:style m))
-     (scale-type      . ,(meter:scale-type m))
-     (is-sharp        . ,(meter:is-sharp m))
-     (glow-multiplier . ,(meter:glow-multiplier m))
-     (range-min       . ,(meter:range-min m))
-     (range-max       . ,(meter:range-max m))
-     (num-segments    . ,(meter:num-segments m))
-     (tick-mode       . ,(meter:tick-mode m)))))
-
-(define-method (component->model (s <scope>))
-  (append
-   (next-method)
-   `((grid-style      . ,(scope:grid-style s))
-     (is-sharp        . ,(scope:is-sharp s))
-     (glow-multiplier . ,(scope:glow-multiplier s)))))
-
-
-
-
-;; ======================================================================
 ;; COMPONENT REGISTRY
 ;;
 ;; *components* continua ad essere il registro centrale.
