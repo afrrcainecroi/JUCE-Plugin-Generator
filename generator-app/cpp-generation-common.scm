7b5b875d8caf830340d8ff9c015bc0d70eecd49e
franco <afrrcainecroi@gmail.com>
2026-08-19 17:25:33 +0200

    Extract C++ generation modules

---
+++ generator-app/cpp-generation-common.scm
@@ -0,0 +1,452 @@
+(define-module (generator-app cpp-generation-common)
+  #:use-module (ice-9 format)
+  #:use-module (srfi srfi-1)
+  #:export (slider-properties->cpp
+            slider-normalisable-range->cpp
+            meter-properties->cpp
+            scope-properties->cpp
+            palette-selector-callback->cpp
+            selector-constructor-code
+            slider-scale->cpp
+            bool->cpp
+            slider-kinetic-properties->cpp
+            rotary-kinetic-properties->cpp
+            tick-labels->cpp
+            cpp-string
+            justification->cpp
+            selector-items->cpp))
+
+(define (error message . args)
+  (scm-error 'misc-error
+             #f
+             (string-append
+              message
+              (apply string-append (map (lambda (_) " ~S") args)))
+             args
+             #f))
+
+(define (slider-properties->cpp model)
+  (let ((var         (assoc-ref model 'var))
+        (title       (assoc-ref model 'title))
+        (value-type  (assoc-ref model 'value-type))
+        (suffix      (assoc-ref model 'suffix))
+        (show-value  (assoc-ref model 'show-value))
+        (show-ticks  (assoc-ref model 'show-ticks))
+        (show-labels (assoc-ref model 'show-labels))
+        (tick-count  (assoc-ref model 'tick-count))
+        (tick-mode   (assoc-ref model 'tick-mode))
+        (tick-labels (assoc-ref model 'tick-labels)))
+
+    (string-append
+
+     (format #f
+"~a.getProperties().set(\"title\", ~a);
+"
+             var
+             (cpp-string title))
+
+     (format #f
+"~a.getProperties().set(\"valueType\", ~a);
+"
+             var
+             (cpp-string
+              (symbol->string value-type)))
+
+     (format #f
+"~a.getProperties().set(\"suffix\", ~a);
+"
+             var
+             (cpp-string suffix))
+
+     (format #f
+"~a.getProperties().set(\"showValue\", ~a);
+"
+             var
+             (bool->cpp show-value))
+
+     (format #f
+"~a.getProperties().set(\"showTicks\", ~a);
+"
+             var
+             (bool->cpp show-ticks))
+
+     (format #f
+"~a.getProperties().set(\"showLabels\", ~a);
+"
+             var
+             (bool->cpp show-labels))
+
+     (format #f
+"~a.getProperties().set(\"tickCount\", ~a);
+"
+             var
+             tick-count)
+
+     (format #f
+"~a.getProperties().set(\"tickMode\", ~a);
+"
+             var
+             (cpp-string
+              (symbol->string tick-mode)))
+
+     ;; tick-labels lo affrontiamo separatamente se non è già emesso.
+     )))
+(define (slider-normalisable-range->cpp model)
+  (let ((min      (assoc-ref model 'min))
+        (max      (assoc-ref model 'max))
+        (interval (assoc-ref model 'interval))
+        (scale    (assoc-ref model 'scale)))
+    (case scale
+      ((linear)
+       (format #f
+               "juce::NormalisableRange<float>(~a, ~a, ~a)"
+               min
+               max
+               interval))
+      ((logarithmic)
+       (when (or (<= min 0)
+                 (<= max 0))
+         (error "Logarithmic parameter requires positive min/max"
+                model))
+       (let ((midpoint (sqrt (* min max))))
+         (format #f
+		 "[] {
+    juce::NormalisableRange<float> range(~a, ~a, ~a);
+    range.setSkewForCentre(~a);
+    return range;
+}()"
+                 min
+                 max
+                 interval
+                 midpoint)))
+      (else
+       (error "Invalid slider scale"
+              scale)))))
+
+(define (meter-properties->cpp model)
+  (let ((var             (assoc-ref model 'var))
+        (style           (assoc-ref model 'style))
+        (scale-type      (assoc-ref model 'scale-type))
+        (is-sharp        (assoc-ref model 'is-sharp))
+        (glow-multiplier (assoc-ref model 'glow-multiplier))
+        (range-min       (assoc-ref model 'range-min))
+        (range-max       (assoc-ref model 'range-max))
+        (num-segments    (assoc-ref model 'num-segments))
+        (tick-mode       (assoc-ref model 'tick-mode)))
+    (string-append
+     (format #f
+             "~a.setStyle(KineticMeter::MeterStyle::~a);~%"
+             var
+             (case style
+               ((segmented) "Segmented")
+               ((analog)    "Analog")))
+     (format #f
+             "~a.properties.set(\"scaleType\", \"~a\");~%"
+             var
+             (cpp-string
+              (symbol->string scale-type)))
+     (if is-sharp
+         (format #f
+                 "~a.properties.set(\"isSharp\", true);~%"
+                 var)
+         "")
+     (format #f
+             "~a.properties.set(\"glowMultiplier\", ~a);~%"
+             var
+             glow-multiplier)
+     (format #f
+             "~a.properties.set(\"rangeMin\", ~a);~%"
+             var
+             range-min)
+     (format #f
+             "~a.properties.set(\"rangeMax\", ~a);~%"
+             var
+             range-max)
+     (format #f
+             "~a.properties.set(\"numSegments\", ~a);~%"
+             var
+             num-segments)
+     (format #f
+             "~a.properties.set(\"tickMode\", \"~a\");~%"
+             var
+             (cpp-string
+              (symbol->string tick-mode))))))
+
+(define (scope-properties->cpp model)
+  (let ((var             (assoc-ref model 'var))
+        (grid-style      (assoc-ref model 'grid-style))
+        (is-sharp        (assoc-ref model 'is-sharp))
+        (glow-multiplier (assoc-ref model 'glow-multiplier)))
+    (string-append
+     (format #f
+             "~a.properties.set(\"gridStyle\", \"~a\");~%"
+             var
+             (cpp-string
+              (symbol->string grid-style)))
+     (if is-sharp
+         (format #f
+                 "~a.properties.set(\"isSharp\", true);~%"
+                 var)
+         "")
+     (format #f
+             "~a.properties.set(\"glowMultiplier\", ~a);~%"
+             var
+             glow-multiplier))))
+
+(define (palette-selector-callback->cpp model)
+  (let ((var
+         (assoc-ref model 'var)))
+    (format #f
+"~a.setWantsKeyboardFocus(false);
+~a.onChange = [this]
+{
+    KineticLookAndFeel::PaletteType type;
+    switch (~a.getSelectedId())
+    {
+        case 1:  type = KineticLookAndFeel::PaletteType::Cyan; break;
+        case 2:  type = KineticLookAndFeel::PaletteType::Plasma; break;
+        case 3:  type = KineticLookAndFeel::PaletteType::Gold; break;
+        case 4:  type = KineticLookAndFeel::PaletteType::Matrix; break;
+        case 5:  type = KineticLookAndFeel::PaletteType::Fire; break;
+        case 6:  type = KineticLookAndFeel::PaletteType::Ocean; break;
+        case 7:  type = KineticLookAndFeel::PaletteType::Toxic; break;
+        case 8:  type = KineticLookAndFeel::PaletteType::Radon; break;
+        case 9:  type = KineticLookAndFeel::PaletteType::White; break;
+        case 10: type = KineticLookAndFeel::PaletteType::Midnight; break;
+        case 11: type = KineticLookAndFeel::PaletteType::Sunset; break;
+        case 12: type = KineticLookAndFeel::PaletteType::Mint; break;
+        case 13: type = KineticLookAndFeel::PaletteType::Vaporwave; break;
+        case 14: type = KineticLookAndFeel::PaletteType::Amber; break;
+        case 15: type = KineticLookAndFeel::PaletteType::Crimson; break;
+        case 16: type = KineticLookAndFeel::PaletteType::Voltage; break;
+        case 17: type = KineticLookAndFeel::PaletteType::Ultraviolet; break;
+        case 18: type = KineticLookAndFeel::PaletteType::Stealth; break;
+        default:
+            type = KineticLookAndFeel::PaletteType::Cyan;
+            break;
+    }
+    kineticLNF.animatePaletteChange(type, 2000);
+    repaint();
+};
+~a.setWantsKeyboardFocus(true);
+"
+            var
+            var
+            var
+            var)))
+
+(define (selector-constructor-code model)
+  (let ((var
+         (assoc-ref model 'var))
+        (items
+         (assoc-ref model 'items))
+        (default-index
+          (assoc-ref model 'default-index)))
+    (string-append
+     (format #f
+             "addAndMakeVisible(~a);~%"
+             var)
+     (selector-items->cpp
+      var
+      items)
+     (format #f
+             "~a.setSelectedId(~a);~%"
+             var
+             default-index))))
+
+(define-public (slider-scale->cpp model)
+  (let ((scale (assoc-ref model 'scale))
+        (var   (assoc-ref model 'var))
+        (min   (assoc-ref model 'min))
+        (max   (assoc-ref model 'max)))
+    (case scale
+      ((linear) "")
+      ((logarithmic)
+       (when (or (not min)
+                 (not max)
+                 (<= min 0)
+                 (<= max 0))
+         (error "Logarithmic slider requires min/max > 0" model))
+       (let ((midpoint
+              (sqrt (* min max))))
+         (format #f "~a.setSkewFactorFromMidPoint(~a);~%" var midpoint)))
+      (else (error "Invalid slider scale" scale)))))
+
+(define-public (bool->cpp b)
+  (if b "true" "false"))
+
+(define-public (slider-kinetic-properties->cpp model)
+  (let ((var         (assoc-ref model 'var))
+        (title       (assoc-ref model 'title))
+        (value-type  (assoc-ref model 'value-type))
+        (suffix      (assoc-ref model 'suffix))
+        (show-value  (assoc-ref model 'show-value))
+        (show-ticks  (assoc-ref model 'show-ticks))
+        (show-labels (assoc-ref model 'show-labels))
+        (tick-count  (assoc-ref model 'tick-count))
+        (tick-mode   (assoc-ref model 'tick-mode))
+        (tick-labels (assoc-ref model 'tick-labels)))
+    (string-append
+     ;; title
+     (if (and title
+              (not (string-null? title)))
+         (format #f
+                 "~a.getProperties().set(\"title\", \"~a\");~%"
+                 var (cpp-string title))
+         "")
+     ;; valueType
+     (if value-type
+         (format #f
+                 "~a.getProperties().set(\"valueType\", \"~a\");~%"
+                 var
+                 (cpp-string (symbol->string value-type)))
+         "")
+     ;; suffix
+     (if (and suffix
+              (not (string-null? suffix)))
+         (format #f
+                 "~a.getProperties().set(\"suffix\", \"~a\");~%"
+                 var (cpp-string suffix))
+         "")
+     ;; showValue
+     (format #f
+             "~a.getProperties().set(\"showValue\", ~a);~%"
+             var
+             (bool->cpp show-value))
+     ;; showTicks:
+     ;; per il rotary la PRESENZA della proprietà significa true.
+     (if show-ticks
+         (format #f
+                 "~a.getProperties().set(\"showTicks\", true);~%"
+                 var)
+         "")
+     ;; showLabels usa invece il valore booleano.
+     (format #f
+             "~a.getProperties().set(\"showLabels\", ~a);~%"
+             var
+             (bool->cpp show-labels))
+     ;; tickCount
+     (if (and tick-count
+              (> tick-count 0))
+         (format #f
+                 "~a.getProperties().set(\"tickCount\", ~a);~%"
+                 var tick-count)
+         "")
+     ;; tickMode
+     (if tick-mode
+         (format #f
+                 "~a.getProperties().set(\"tickMode\", \"~a\");~%"
+                 var
+                 (cpp-string (symbol->string tick-mode)))
+         "")
+     ;; tickLabels
+     (tick-labels->cpp model))))
+
+(define-public (rotary-kinetic-properties->cpp model)
+  (format #t
+          "ROTARY ~s icon-set=~s morph-icon=~s~%"
+          (assoc-ref model 'id)
+          (assoc-ref model 'icon-set)
+          (assoc-ref model 'morph-icon))
+  (let ((var        (assoc-ref model 'var))
+        (icon-type  (assoc-ref model 'icon-type))
+        (morph-icon (assoc-ref model 'morph-icon))
+        (icon-set   (assoc-ref model 'icon-set)))
+    (string-append
+     ;; iconType
+     (if (and icon-type
+              (>= icon-type 0))
+         (format #f
+                 "~a.getProperties().set(\"iconType\", ~a);~%"
+                 var
+                 icon-type)
+         "")
+     ;; morphIcon:
+     ;; nel KineticLookAndFeel conta la PRESENZA della proprietà.
+     (if morph-icon
+         (format #f
+                 "~a.getProperties().set(\"morphIcon\", true);~%"
+                 var)
+         "")
+     ;; iconSet
+     (if (and icon-set
+              (not (string-null? icon-set)))
+         (format #f
+                 "~a.getProperties().set(\"iconSet\", \"~a\");~%"
+                 var
+                 (cpp-string icon-set))
+         ""))))
+
+(define-public (tick-labels->cpp model)
+  (let ((var         (assoc-ref model 'var))
+        (tick-labels (assoc-ref model 'tick-labels)))
+    (if (or (not tick-labels)
+            (null? tick-labels))
+        ""
+        (let ((array-var
+               (string-append var "TickLabels")))
+          (string-append
+           (format #f
+                   "juce::Array<juce::var> ~a;~%"
+                   array-var)
+           (apply string-append
+                  (map
+                   (lambda (label)
+                     (format #f
+                             "~a.add(\"~a\");~%"
+                             array-var
+                             (cpp-string label)))
+                   tick-labels))
+           (format #f
+                   "~a.getProperties().set(\"tickLabels\", juce::var(~a));~%"
+                   var
+                   array-var))))))
+
+(define-public (cpp-string s)
+  (let ((out (open-output-string)))
+    (string-for-each
+     (lambda (c)
+       (case c
+         ((#\\)
+          (display "\\\\" out))
+         ((#\")
+          (display "\\\"" out))
+         ((#\newline)
+          (display "\\n" out))
+         ((#\return)
+          (display "\\r" out))
+         ((#\tab)
+          (display "\\t" out))
+         (else
+          (write-char c out))))
+     s)
+    (get-output-string out)))
+
+(define (justification->cpp justification)
+  (case justification
+    ((centred)
+     "juce::Justification::centred")
+    ((centred-left)
+     "juce::Justification::centredLeft")
+    ((centred-right)
+     "juce::Justification::centredRight")
+    ((left)
+     "juce::Justification::left")
+    ((right)
+     "juce::Justification::right")
+    (else
+     (error "Invalid label justification"
+            justification))))
+
+(define-public (selector-items->cpp var items)
+  (apply
+   string-append
+   (map
+    (lambda (item index)
+      (format #f
+              "~a.addItem(\"~a\", ~a);~%"
+              var
+              (cpp-string item)
+              index))
+    items
+    (iota (length items) 1))))
---
+++ generator-app/cpp-generation.scm
@@ -0,0 +1,437 @@
+(define-module (generator-app cpp-generation)
+  #:use-module (ice-9 format)
+  #:use-module (oop goops)
+  #:use-module (generator-app generation-protocols)
+  #:use-module (generator-app dsl-model)
+  #:use-module (generator-app registration)
+  #:use-module (generator-app cpp-generation-common)
+  #:re-export (component->member-declaration
+               model->member-declaration
+               model->constructor-code
+               model->attachment-declaration
+               model->attachment-code
+               model->parameter-code
+               model->dparams-code
+               model->getparams-code
+               model->valueparams-code
+               model->destroy-code))
+
+(define (error message . args)
+  (scm-error 'misc-error
+             #f
+             (string-append
+              message
+              (apply string-append (map (lambda (_) " ~S") args)))
+             args
+             #f))
+
+(define-method (component->member-declaration (s <slider>))
+  (format #f
+          "juce::Slider ~a;~%"
+          (component-cpp-var s)))
+
+(define-method (model->member-declaration (model <list>))
+  (let ((type (assoc-ref model 'type))
+        (var  (assoc-ref model 'var)))
+    (case type
+      ((rotary-slider linear-slider)
+       (format #f
+               "juce::Slider ~a;~%"
+               var))
+      ((selector palette-selector)
+       (format #f
+               "juce::ComboBox ~a;~%"
+               var))
+      ((text-button)
+       (format #f
+               "juce::TextButton ~a;~%"
+               var))
+      ((toggle-button switch bypass-switch)
+       (format #f
+               "juce::ToggleButton ~a;~%"
+               var))
+      ((meter)
+       (format #f
+               "KineticMeter ~a;~%"
+               var))
+      ((scope)
+       (format #f
+               "KineticScope ~a;~%"
+               var))
+      ((label header footer link palette-label)
+       (format #f
+               "juce::Label ~a;~%"
+               var))
+      (else ""))))
+
+(define-method (model->constructor-code (model <list>))
+  (let ((type        (assoc-ref model 'type))
+        (var         (assoc-ref model 'var))
+        (min         (assoc-ref model 'min))
+        (max         (assoc-ref model 'max))
+        (interval    (assoc-ref model 'interval))
+        (orientation (assoc-ref model 'orientation)))
+    (case type
+      ((rotary-slider)
+       (string-append
+        (format #f
+                "addAndMakeVisible(~a);~%"
+                var)
+        (format #f
+                "~a.setRange(~a, ~a, ~a);~%"
+                var min max interval)
+        (format #f
+                "~a.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);~%"
+                var)
+	;; Il valore viene disegnato dal KineticLookAndFeel.
+	;; Eliminiamo il TextBox standard JUCE.
+	(format #f
+		"~a.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);~%"
+		var)
+
+        (slider-scale->cpp model)
+	(slider-kinetic-properties->cpp model)
+	(rotary-kinetic-properties->cpp model)))
+      ((linear-slider)
+       (string-append
+        (format #f
+                "addAndMakeVisible(~a);~%"
+                var)
+        (format #f
+                "~a.setRange(~a, ~a, ~a);~%"
+                var min max interval)
+        (format #f
+                "~a.setSliderStyle(~a);~%"
+                var
+                (case orientation
+                  ((horizontal)
+                   "juce::Slider::LinearHorizontal")
+                  ((vertical)
+                   "juce::Slider::LinearVertical")
+                  (else
+                   (error
+                    "Invalid linear slider orientation"
+                    orientation))))
+	;; Il valore viene disegnato dal KineticLookAndFeel.
+	(format #f
+		"~a.setTextBoxStyle(juce::Slider::NoTextBox, false, 0, 0);~%"
+		var)
+        (slider-scale->cpp model)
+	(slider-kinetic-properties->cpp model)))
+      ;; ((selector)
+      ;;  (let ((items
+      ;;         (assoc-ref model 'items))
+      ;; 	     (default-index
+      ;;          (assoc-ref model 'default-index)))
+      ;; 	 (string-append
+      ;; 	  (format #f
+      ;; 		  "addAndMakeVisible(~a);~%"
+      ;; 		  var)
+      ;; 	  (selector-items->cpp
+      ;; 	   var
+      ;; 	   items)
+      ;; 	  (format #f
+      ;; 		  "~a.setSelectedItemIndex(~a, juce::dontSendNotification);~%"
+      ;; 		  var
+      ;; 		  default-index))))
+      ((text-button)
+       (let ((text
+              (assoc-ref model 'text)))
+	 (string-append
+	  (format #f
+		  "addAndMakeVisible(~a);~%"
+		  var)
+	  (format #f
+		  "~a.setButtonText(\"~a\");~%"
+		  var
+		  (cpp-string text)))))
+      ((toggle-button switch bypass-switch)
+       (let ((text
+              (assoc-ref model 'text))
+	     (default-state
+               (assoc-ref model 'default-state))
+	     (style
+		 (assoc-ref model 'style))
+	     (tooltip
+              (assoc-ref model 'tooltip))
+	     (role
+	      (assoc-ref model 'role))
+	     )
+	 (string-append
+	  (format #f
+		  "addAndMakeVisible(~a);~%"
+		  var)
+	  (format #f
+		  "~a.setButtonText(\"~a\");~%"
+		  var
+		  (cpp-string text))
+	  (format #f
+		  "~a.setToggleState(~a, juce::dontSendNotification);~%"
+		  var
+		  (bool->cpp default-state))
+	  (if (memq role '(bypass dsp-bypass))
+	      (format #f
+		      "~a.onStateChange = [this] { repaint(); };~%"
+		      var)
+	      "")
+	  (if (eq? style 'switch)
+              (format #f
+                      "~a.getProperties().set(\"style\", \"switch\");~%"
+                      var)
+              "")
+	      (if (and tooltip
+             (not (string-null? tooltip)))
+        (format #f
+                "~a.setTooltip(\"~a\");~%"
+                var
+                (cpp-string tooltip))
+        ""))))
+      ((meter)
+       (string-append
+	(format #f
+		"addAndMakeVisible(~a);~%"
+		var)
+	(meter-properties->cpp model)))
+      ((scope)
+       (string-append
+	(format #f
+		"addAndMakeVisible(~a);~%"
+		var)
+	(scope-properties->cpp model)))
+      ((header)
+       (let ((text (assoc-ref model 'text)))
+	 (string-append
+	  (format #f
+		  "addAndMakeVisible(~a);~%"
+		  var)
+	  (format #f
+		  "~a.setText(\"~a\", juce::dontSendNotification);~%"
+		  var
+		  (cpp-string text))
+	  (format #f
+		  "~a.setFont(juce::FontOptions(32.0f).withStyle(\"Bold\"));~%"
+		  var)
+	  (format #f
+		  "~a.setJustificationType(juce::Justification::centred);~%"
+		  var)
+	  (format #f
+		  "~a.setColour(juce::Label::textColourId, kineticLNF.currentPalette.neonWhite);~%"
+		  var))))
+      ((footer)
+       (let ((text (assoc-ref model 'text)))
+	 (string-append
+	  (format #f
+		  "addAndMakeVisible(~a);~%"
+		  var)
+	  (format #f
+		  "~a.setText(\"~a\", juce::dontSendNotification);~%"
+		  var
+		  (cpp-string text))
+	  (format #f
+		  "~a.setName(\"~a\");~%"
+		  var
+		  (cpp-string var))
+	  (format #f
+		  "~a.setFont(juce::FontOptions(12.0f));~%"
+		  var)
+	  (format #f
+		  "~a.setJustificationType(juce::Justification::bottomRight);~%"
+		  var)
+	  (format #f
+		  "~a.setColour(juce::Label::textColourId, juce::Colours::grey);~%"
+		  var))))
+      ((link)
+       (let ((text (assoc-ref model 'text)))
+	 (string-append
+	  (format #f
+		  "addAndMakeVisible(~a);~%"
+		  var)
+	  (format #f
+		  "~a.setText(\"~a\", juce::dontSendNotification);~%"
+		  var
+		  (cpp-string text))
+	  (format #f
+		  "~a.setName(\"~a\");~%"
+		  var
+		  (cpp-string var))
+	  (format #f
+		  "~a.setFont(juce::FontOptions(12.0f));~%"
+		  var)
+	  (format #f
+		  "~a.setJustificationType(juce::Justification::bottomLeft);~%"
+		  var)
+	  (format #f
+		  "~a.setColour(juce::Label::textColourId, juce::Colours::grey);~%"
+		  var)
+	  (format #f
+		  "~a.setMinimumHorizontalScale(1.0f);~%"
+		  var)
+	  (format #f
+		  "~a.setMouseCursor(juce::MouseCursor::PointingHandCursor);~%"
+		  var)
+	  (format #f
+		  "~a.addMouseListener(this, false);~%"
+		  var))))
+      ((selector)
+       (selector-constructor-code model))
+      ((palette-selector)
+       (string-append
+        (selector-constructor-code model)
+        (palette-selector-callback->cpp model)))
+      ((label palette-label)
+       (let ((text
+              (assoc-ref model 'text))
+	     (justification
+              (assoc-ref model 'justification)))
+	 (string-append
+	  (format #f
+		  "addAndMakeVisible(~a);~%"
+		  var)
+	  (format #f
+		  "~a.setText(\"~a\", juce::dontSendNotification);~%"
+		  var
+		  (cpp-string text))
+	  (format #f
+		  "~a.setJustificationType(~a);~%"
+		  var
+		  (justification->cpp justification)))))
+      (else ""))))
+
+(define-method (model->attachment-declaration (model <list>))
+  (let ((type (assoc-ref model 'type))
+        (var  (assoc-ref model 'var)))
+    (cond
+     ((button-parameter-type? type)
+      (format #f
+              "std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> ~aAttachment;~%"
+              var))
+
+     ((slider-parameter-type? type)
+      (format #f
+              "std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> ~aAttachment;~%"
+              var))
+
+     (else ""))
+    ;; (case type
+    ;;   ((bypass-switch)
+    ;;    (format #f
+    ;; 	       "std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> ~aAttachment;~%"
+    ;;            var))
+    ;;   ((rotary-slider linear-slider)
+    ;;    (format #f
+    ;;            "std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> ~aAttachment;~%"
+    ;;            var))
+    ;;   (else ""))
+    ))
+
+(define-method (model->attachment-code (model <list>))
+  (let ((type         (assoc-ref model 'type))
+        (var          (assoc-ref model 'var))
+        (parameter-id (assoc-ref model 'parameter-id)))
+
+    (cond
+     ((button-parameter-type? type)
+      (format #f
+"~aAttachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(
+    ap.parameters,
+    \"~a\",
+    ~a
+);~%"
+              var
+              (cpp-string parameter-id)
+              var))
+
+     ((slider-parameter-type? type)
+      (format #f
+"~aAttachment = std::make_unique<juce::AudioProcessorValueTreeState::SliderAttachment>(
+    ap.parameters,
+    \"~a\",
+    ~a
+);~%"
+              var
+              (cpp-string parameter-id)
+              var))
+
+     (else ""))))
+
+(define-method (model->parameter-code (model <list>))
+  (let ((type           (assoc-ref model 'type))
+        (parameter-id   (assoc-ref model 'parameter-id))
+        (parameter-name (assoc-ref model 'parameter-name))
+        (default-state  (assoc-ref model 'default-state))
+        (version-hint   (assoc-ref model 'version-hint))
+        (default        (assoc-ref model 'default)))
+
+    (cond
+     ((button-parameter-type? type)
+      (format #f
+"params.push_back(
+    std::make_unique<juce::AudioParameterBool>(
+        juce::ParameterID { \"~a\", ~a },
+        \"~a\",
+        ~a));~%"
+              (cpp-string parameter-id)
+              version-hint
+              (cpp-string parameter-name)
+              (bool->cpp default-state)))
+
+     ((slider-parameter-type? type)
+      (format #f
+"params.push_back(
+    std::make_unique<juce::AudioParameterFloat>(
+        juce::ParameterID { \"~a\", ~a },
+        \"~a\",
+        ~a,
+        ~a));~%"
+              (cpp-string parameter-id)
+              version-hint
+              (cpp-string parameter-name)
+              (slider-normalisable-range->cpp model)
+              default))
+
+     (else ""))))
+
+(define-method (model->dparams-code (model <list>))
+  (let ((type      (assoc-ref model 'type))
+        (reference (assoc-ref model 'processor-reference)))
+
+    (if (parameter-component-type? type)
+        (format #f
+"std::atomic<float>* param_~a = nullptr;
+float value_~a;~%"
+                reference
+                reference)
+        "")))
+
+(define-method (model->getparams-code (model <list>))
+  (let ((type         (assoc-ref model 'type))
+        (reference    (assoc-ref model 'processor-reference))
+        (parameter-id (assoc-ref model 'parameter-id)))
+
+    (if (parameter-component-type? type)
+        (format #f
+"param_~a = parameters.getRawParameterValue(\"~a\");~%"
+                reference
+                (cpp-string parameter-id))
+        "")))
+
+(define-method (model->valueparams-code (model <list>))
+  (let ((type      (assoc-ref model 'type))
+        (reference (assoc-ref model 'processor-reference)))
+
+    (if (parameter-component-type? type)
+        (format #f
+"value_~a = param_~a->load();~%"
+                reference
+                reference)
+        "")))
+
+(define-method (model->destroy-code (model <list>))
+  (let ((type (assoc-ref model 'type))
+        (var  (assoc-ref model 'var)))
+    (case type
+      ((bypass-switch rotary-slider linear-slider)
+       (format #f
+"~aAttachment.reset();~%"
+               var))
+      (else ""))))
